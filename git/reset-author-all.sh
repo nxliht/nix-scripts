ecc378b5290cb2c99a8bb6b8e30544ddb7adee78
Michel Ocon <nxlith@gmail.com>
2018-05-30 20:59:56 -0500

    Add git scripts

---
+++ git/reset-author-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo 'Type the name to be replaced'
+read OLD_NAME
+echo 'Type the new to name'
+read NEW_NAME
+echo 'Type the new email'
+read NEW_EMAIL
+
+export OLD_NAME
+export NEW_NAME
+export NEW_EMAIL
+
+git filter-branch -f --env-filter '
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]; then
+    GIT_AUTHOR_NAME=$NEW_NAME
+    GIT_AUTHOR_EMAIL=$NEW_EMAIL
+    GIT_COMMITTER_NAME=$NEW_NAME
+    GIT_COMMITTER_EMAIL=$NEW_EMAIL
+fi
+' -- --all
+
